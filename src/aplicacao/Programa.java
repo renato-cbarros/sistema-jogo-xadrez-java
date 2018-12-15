5bbebe981913fdddc3b7584ed9c99efc70a39f4d
Renato Barros <renatobarros123@outlook.com.br>
2018-12-15 14:04:01 -0200

    Criando repositorio

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello word!");
+
+	}
+
+}
