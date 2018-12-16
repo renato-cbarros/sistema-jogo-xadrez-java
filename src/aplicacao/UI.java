7a6f387e8938de711bbba4daa97497771ee71f59
Renato Barros <renatobarros123@outlook.com.br>
2018-12-15 22:11:57 -0200

    Logica para mover peças

--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -1,7 +1,11 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import xadrez.Cor;
 import xadrez.PecaXadrez;
+import xadrez.PosicaoXadrez;
 
 public class UI {
 	
@@ -25,6 +29,18 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	public static PosicaoXadrez lerPosicaoXadrez(Scanner sc) {
+		try {
+		String s = sc.nextLine();
+		char coluna = s.charAt(0);
+		int linha = Integer.parseInt(s.substring(1));
+		return new PosicaoXadrez(coluna, linha);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Erro ao ler a PosicaoXadrez. Valores validos de a1 a h8.");
+		}
+	}
 	
 	public static void mostraTabuleiro(PecaXadrez[][] pecas) {
 		
